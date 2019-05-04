94bf3e2d7bf810c5d5055df7804bf0c4cd87d5e9
mateuslucas97 <31663843+mateuslucas97@users.noreply.github.com>
2019-05-04 15:37:24 -0300

    Update somador_1bit.v

--- somador_1bit/somador_1bit.v
+++ somador_1bit/somador_1bit.v
@@ -1,6 +1,5 @@
-//Somador de 1 bit com resposta no display de 7 segmentos
-
 module somador_1bit(X,Y, TE, HEX0);
+
 input X,Y,TE; //entradas de controle(X e Y são 1bit cada)e o transporte de entrada
 output [0:6]HEX0; //saida no display de 7 segmentos ( é um vetor de 7 bits)
 
@@ -11,10 +10,11 @@ assign S = ((X)^(Y)^(TE)); // expressão booleana para a a soma
 assign TS = ((X&Y)|(X&TE)|(Y&TE)); //expressão booleana para o TS
 
 //expressões para cada bit que será mostrada no dislay de 7 segmentos
-assign E[3] = 0;
-assign E[2] = 0;
-assign E[1] = TS;
-assign E[0] = S;
+  assign E[3] = 0;
+  assign E[2] = 0;
+  assign E[1] = TS;
+  assign E[0] = S;
+  
 //expressão para chamar o decodificador e mostrar o resultado
 decodificador(E, HEX0);
 
