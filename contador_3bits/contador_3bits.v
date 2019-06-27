da46e8b674ef214e55cd0bf76c22bc2e79dd47a8
mateuslucas97 <31663843+mateuslucas97@users.noreply.github.com>
2019-06-26 21:11:58 -0300

    Update contador_3bits.v

--- contador_3bits/contador_3bits.v
+++ contador_3bits/contador_3bits.v
@@ -1,17 +1,20 @@
-module contador_3bits(CLOCK_50,reset,HEX0);
+module contador_3bits(CLOCK_50,reset,HEX0);//entradas e saidas do circuito
 
-input CLOCK_50, reset;
-output [0:6] HEX0;
+input CLOCK_50, reset; //entradas (CLOCK_50 é um nome reservado pelo quartus, ou seja, já faz a pinagem automaticamente
+output [0:6] HEX0;//saida do circuito, HEX0  também é nome reservado pelo quartus
 
-reg [2:0] q;
-wire 	clock;
+reg [2:0] q;//sinal interno para mudança de estados (aqui são 3 bits, que é a entrada do circuito)
+wire 	clock;//fio que vai concetar o clock do divisor de frequenci aqui
 	
+	//começa q com zero
 	initial begin
 		q = 3'b000;
 		end
-		
+	
+	//chamada do modulo divisor de frequencia aplicado nas variaveis deste arquivo
 	divisor_freq dv(clock, reset, CLOCK_50);
-		
+	
+	//mesmo esquema do divisor de frequencia
 	always @(posedge clock or posedge reset)
 	begin
 		if(reset == 1'b1)
@@ -25,7 +28,7 @@ wire 	clock;
 		end
 	end
 	
-	
+	//a saida vai aparecer no decodificador levando em consideração a variavel interna q
 	decodificador d1(q,HEX0[0:6]);
 
 endmodule
